d8e41b5c0a850e4727974f73b78b759566293359
“OnenessPriya” <priyamlk2505@gmail.com>
2023-02-24 15:21:51 +0530

    test

--- resources/views/site/postcode/detail.blade.php
+++ resources/views/site/postcode/detail.blade.php
@@ -70,7 +70,7 @@
     @endphp
 
     <section class="inner_banner"
-        {{-- @if($postcode_img->image)
+        @if($postcode_img->image)
             style="background: url({{asset('/admin/uploads/suburb/'.$postcode_img->image)}})"
         @else
             @if($data->image)
@@ -82,7 +82,7 @@
             style="background: url({{asset('Directory/placeholder-image.png')}})"
             @endif
             @endif
-        @endif --}}
+        @endif
         >
         <div class="container position-relative">
             <h1>{{ $data ? $data->pin : '' }}</h1>
@@ -269,7 +269,7 @@
                         </div>
 
                             {{-- review like/ dislike --}}
-                            @guest
+                            @if(Auth::guard('user')->check())
                                 <a href="javascript:void(0)" class="location_btn ms-auto"
                                 onclick="reviewLike({{ $data->id }})"
                                 title="Like">
@@ -332,7 +332,7 @@
                                     <svg id="reviewlikeBtn_{{ $data->id }}_grid" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-down"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
                                     <span>{{ CountDisLikeReview($data->id)  }}</span>
                                 </a>
-                            @endguest
+                            @endif
                     </div>
                 </div>
             </div>
